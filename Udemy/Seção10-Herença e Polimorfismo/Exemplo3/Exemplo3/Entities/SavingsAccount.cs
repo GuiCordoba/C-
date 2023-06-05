7807951188fb610826f42a5dc39b312c316494dc
Guilherme <guimcordoba@hotmail.com>
2023-06-05 16:43:53 -0300

    Criação exemplo3 classes e métodos selados

---
+++ Udemy/Seção10-Herença e Polimorfismo/Exemplo3/Exemplo3/Entities/SavingsAccount.cs
@@ -0,0 +1,30 @@
+﻿
+
+namespace Exemplo3.Entities
+{
+    sealed class SavingsAccount : Account // com o 'sealed' não é mais possivel criar derivações da classe SavingsAccount
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount()
+        {
+        }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate)
+            : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+
+        public sealed override void Withdraw(double amount) // com o sealed, não sera mais possivel sobescrever o método Withdraw
+        {
+            base.Withdraw(amount);
+            Balance -= 2.0;
+        }
+    }
+}
---
+++ Udemy/Seção10-Herença e Polimorfismo/Exemplo3/Exemplo3/Program.cs
@@ -0,0 +1,11 @@
+﻿using Exemplo3.Entities;
+namespace Exemplo3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
