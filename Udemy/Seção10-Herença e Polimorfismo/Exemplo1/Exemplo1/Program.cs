d6133451943de138c15633894729beef790394de
Guilherme <guimcordoba@hotmail.com>
2023-06-05 11:38:11 -0300

    aplicando Upcasting Downcasting exemplo 1

--- Udemy/Seção10-Herença e Polimorfismo/Exemplo1/Exemplo1/Program.cs
+++ Udemy/Seção10-Herença e Polimorfismo/Exemplo1/Exemplo1/Program.cs
@@ -7,9 +7,27 @@ namespace Exemplo1
     {
         static void Main(string[] args)
         {
-            BusinessAccount account = new BusinessAccount(0210, "Guilherme Cordoba", 350.00, 2000.00);
+            Account acc = new Account(1001, "Alex", 0.0);
 
-            Console.WriteLine(account.Holder);
+            BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+
+            // UPCASTING
+
+            Account acc1 = bacc;
+            Account acc2 = new BusinessAccount(1003, "BOb", 0.0, 200.0);
+            Account acc3 = new SavingsAccount(1004, "Anna", 0.0, 0.01);
+
+            //DOWNCASTING
+            BusinessAccount acc4 = (BusinessAccount) acc2;
+            acc4.Loan(100.0);
+
+                //BusinessAccount acc5 = (BusinessAccount) acc3; // ira dar erro, SavingAccount não é compativel com BusinessAcount, segue a solução abaixo:
+
+            if (acc3 is BusinessAccount)
+                {
+                    //BusinessAccount acc5 = (BusinessAccount) acc3;
+                    BusinessAccount acc5 =  acc3 as BusinessAccount; //Sintaxe Alternativa
+                }
         }
     }
 }
