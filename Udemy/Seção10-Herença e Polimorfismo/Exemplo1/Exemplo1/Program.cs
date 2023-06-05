4cff97ded72b144d53d2c1f30ac45480822aed84
Guilherme <guimcordoba@hotmail.com>
2023-06-05 11:09:57 -0300

    Inclusão da pasta  seção 10 e exemplo 1

---
+++ Udemy/Seção10-Herença e Polimorfismo/Exemplo1/Exemplo1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Exemplo1.Entities;
+
+namespace Exemplo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessAccount account = new BusinessAccount(0210, "Guilherme Cordoba", 350.00, 2000.00);
+
+            Console.WriteLine(account.Holder);
+        }
+    }
+}
