b589c6328e47de4a0b0feaf858347cfc6856b574
Guilherme <guimcordoba@hotmail.com>
2023-06-09 17:05:52 -0300

    Adição do exercicio fixação 2, exercicio fixação 1 melhorado

---
+++ Udemy/Seção11-Tratamento de Exeções/ExercicioFixação2/ExercicioFixação2/Program.cs
@@ -0,0 +1,46 @@
+﻿using ExercicioFixação2.Entities;
+
+namespace ExercicioFixação2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Room Number: ");
+            int roomNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Check-in date (dd/MM/yyyy: ");
+            DateTime checkIn = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Check-out date (dd/MM/yyyy: ");
+            DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+            if (checkOut <= checkIn)
+            {
+                Console.WriteLine("Error in reservation: Check-out date must ahead of Check-in date");
+            }
+            else
+            {
+                Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
+                Console.WriteLine("Reservation: " + reservation);
+                Console.WriteLine();
+                Console.WriteLine("Enter data to update reservation: ");
+                Console.WriteLine("Check-in date (dd/MM/yyyy: ");
+                checkIn = DateTime.Parse(Console.ReadLine());
+                Console.WriteLine("Check-out date (dd/MM/yyyy: ");
+                checkOut = DateTime.Parse(Console.ReadLine());
+
+                string error = reservation.UpdateDates(checkIn, checkOut);
+                if (error != null)
+                {
+                    Console.WriteLine("Error in reservation: " + error);
+                }
+                else
+                {
+                    reservation.UpdateDates(checkIn, checkOut);
+                    Console.WriteLine("Reservation: " + reservation);
+                }
+            }
+
+
+        }
+    }
+}
---
+++ Udemy/Seção11-Tratamento de Exeções/ExercicioFixação2/ExercicioFixação2/Reservation.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+
+namespace ExercicioFixação2.Entities
+{
+    internal class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public Reservation()
+        {
+        }
+
+        public Reservation(int roomNumber, DateTime checkin, DateTime checkout)
+        {
+            RoomNumber = roomNumber;
+            CheckIn = checkin;
+            CheckOut = checkout;
+        }
+
+        public int Duration()
+        {
+
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+            return (int)duration.TotalDays;
+        }
+        public String UpdateDates(DateTime checkIn, DateTime checkOut)
+        {
+            DateTime now = DateTime.Now;
+            if (checkIn < now || checkOut < now)
+            {
+               return "Error in reservation: Reservation dates for update must be future dates";
+
+            }
+            if (checkOut <= checkIn)
+            {
+                return "Error in reservation: Check-out date must ahead of Check-in date";
+            }
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+            return null;
+        }
+
+        public override string ToString()
+        {
+            return "Room "
+                + RoomNumber
+                + ", CheckIn: "
+                + CheckIn.ToString("dd/MM/yyyy")
+                + ", checkout: "
+                + CheckOut.ToString("dd/MM/yyyy")
+                + ", "
+                + Duration()
+                + " nights.";
+        }
+    }
+}
