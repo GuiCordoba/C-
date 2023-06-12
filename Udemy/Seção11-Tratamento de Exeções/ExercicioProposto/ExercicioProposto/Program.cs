306320b688269aeb9bd0b72af0da727f88bf6152
Guilherme <guimcordoba@hotmail.com>
2023-06-12 15:38:55 -0300

    Finalização do exercicio de fixação

--- Udemy/Seção11-Tratamento de Exeções/ExercicioProposto/ExercicioProposto/Program.cs
+++ Udemy/Seção11-Tratamento de Exeções/ExercicioProposto/ExercicioProposto/Program.cs
@@ -1,10 +1,41 @@
-﻿namespace ExercicioProposto
+﻿using ExercicioProposto.Entitties;
+using System.Globalization;
+using System;
+using ExercicioProposto.Entitties.Exceptions;
+
+namespace ExercicioProposto
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("Enter account data:");
+            Console.Write("Number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Holder: ");
+            string holder = Console.ReadLine();
+            Console.Write("Initial balance: ");
+            double initialBalance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Withdraw limit: ");
+            double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Account account = new Account(number, holder, initialBalance, withdrawLimit);
+
+            Console.WriteLine("");
+
+            Console.Write("Enter the amount of withdraw: ");
+            double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            try
+            {
+                account.Withdraw(amount);
+                Console.WriteLine("-> New balance: " + account.Balance);
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine("Error in withdraw: " + e.Message);
+            }
+
         }
     }
 }
